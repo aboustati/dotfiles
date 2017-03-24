1b6ac36c702bbe29708396dea9eff6edfb23945e
Ayman Boustati <ayman.boustati@outlook.com>
2017-03-24 11:32:56 +0000

    removed ssh config

--- install.sh
+++ install.sh
@@ -12,4 +12,3 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -sfv "$DOTFILES_DIR/vim/vimrc" ~/.vimrc
 ln -sfv "$DOTFILES_DIR/tmux/tmux.conf" ~/.tmux.conf
 ln -sfv "$DOTFILES_DIR/zsh/zshrc" ~/.zshrc
-ln -sfv "$DOTFILES_DIR/ssh/ssh.config" ~/.ssh/config
