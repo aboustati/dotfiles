b4fc172c71a4d442c2a6fd6c3d5572717ee4d0e7
Ayman Boustati <ayman.boustati@outlook.com>
2017-03-25 21:01:30 +0000

    changed oh my zsh installation

--- install.sh
+++ install.sh
@@ -4,11 +4,16 @@
 export DOTFILES_DIR 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Install Oh My Zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Install Vundle
+mkdir ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 # Bunch of symlinks
 ln -sfv "$DOTFILES_DIR/git/gitconfig" ~/.gitconfig
 #ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
-mkdir ~/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -sfv "$DOTFILES_DIR/vim/vimrc" ~/.vimrc
 ln -sfv "$DOTFILES_DIR/tmux/tmux.conf" ~/.tmux.conf
 ln -sfv "$DOTFILES_DIR/zsh/zshrc" ~/.zshrc
